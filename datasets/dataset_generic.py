528c840cd2f57489e418456d896b6b71a695e31c
Haniya075 <129097930+Haniya075@users.noreply.github.com>
2024-07-04 15:58:36 +0500

    Update dataset_generic.py

--- datasets/dataset_generic.py
+++ datasets/dataset_generic.py
@@ -15,7 +15,7 @@ import h5py
 from utils.utils import generate_split, nth
 
 def save_splits(split_datasets, column_keys, filename, boolean_style=False):
-	splits = [split_datasets[i].slide_data['slide_id'] for i in range(len(split_datasets))]
+	splits = [split_datasets[i].slide_data['slide_id'][:12] for i in range(len(split_datasets))]
 	if not boolean_style:
 		df = pd.concat(splits, ignore_index=True, axis=1)
 		df.columns = column_keys
